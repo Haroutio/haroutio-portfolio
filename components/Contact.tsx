2fc7a84e361b7057724e2dd3b47ae6907a1c56bd
Harout Terterian <hyecartoon@gmail.com>
2023-02-24 18:34:07 -0800

    Added msg for successful contact form submission

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -12,7 +12,7 @@ function Contact() {
   const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState("");
   const { executeRecaptcha } = useGoogleReCaptcha();
-
+  const [success, setSuccess] = useState(false);
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (!fields.name || !fields.email || !fields.phone || !fields.message) {
@@ -41,7 +41,7 @@ function Contact() {
     const data = await response.json();
     setSubmitting(false);
     if (data.success) {
-      console.log("Message sent successfully!");
+      setSuccess(true);
       setFields({
         name: "",
         email: "",
@@ -78,83 +78,94 @@ function Contact() {
         }}
         className="pageInfo-container"
       >
-        <form onSubmit={handleSubmit} className="w-full max-w-lg">
-          <h1>Contact</h1>
-          <div className="mx-auto mb-4">
-            <label htmlFor="name" className="block mb-2 font-bold">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={fields.name}
-              onChange={handleFieldChange}
-              className="w-full rounded-sm max-w-s input input-bordered input-primary"
-              required
-            />
+        {success && (
+          <div className="mt-16">
+            <h1>Message Sent Successfully</h1>
+            <p>
+              Thank you for reaching out. I&apos;ll get back to you as soon as
+              possible.
+            </p>
           </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-2 font-bold ">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={fields.email}
-              onChange={handleFieldChange}
-              className="w-full rounded-sm max-w-s input input-bordered input-primary"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="phone" className="block mb-2 font-bold ">
-              Phone
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              value={fields.phone}
-              onChange={handleFieldChange}
-              className="w-full rounded-sm max-w-s input input-bordered input-primary"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="block mb-2 font-bold ">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              value={fields.message}
-              onChange={handleFieldChange}
-              className="w-full h-24 rounded-sm max-w-s input input-bordered input-primary"
-              required
-            />
-          </div>
-          <div className="flex items-center justify-center mb-4">
-            <input
-              type="hidden"
-              name="recaptcha"
-              id="recaptcha"
-              data-callback="onSubmit"
-              data-action="contact"
-            />
-            {error && <div className="text-red-500">{error}</div>}
-          </div>
-          <div className="flex items-center justify-center">
-            <button
-              type="submit"
-              className="px-4 py-2 font-bold text-white rounded bg-primary hover:bg-primary-focus focus:outline-none focus:shadow-outline"
-              disabled={submitting}
-            >
-              {submitting ? "Submitting..." : "Send Message"}
-            </button>
-          </div>
-        </form>
+        )}
+        {!success && (
+          <form onSubmit={handleSubmit} className="w-full max-w-lg">
+            <h1>Contact</h1>
+            <div className="mx-auto mb-4">
+              <label htmlFor="name" className="block mb-2 font-bold">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={fields.name}
+                onChange={handleFieldChange}
+                className="w-full rounded-sm max-w-s input input-bordered input-primary"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-2 font-bold ">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={fields.email}
+                onChange={handleFieldChange}
+                className="w-full rounded-sm max-w-s input input-bordered input-primary"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="phone" className="block mb-2 font-bold ">
+                Phone
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                value={fields.phone}
+                onChange={handleFieldChange}
+                className="w-full rounded-sm max-w-s input input-bordered input-primary"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="message" className="block mb-2 font-bold ">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={fields.message}
+                onChange={handleFieldChange}
+                className="w-full h-24 rounded-sm max-w-s input input-bordered input-primary"
+                required
+              />
+            </div>
+            <div className="flex items-center justify-center mb-4">
+              <input
+                type="hidden"
+                name="recaptcha"
+                id="recaptcha"
+                data-callback="onSubmit"
+                data-action="contact"
+              />
+              {error && <div className="text-red-500">{error}</div>}
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                type="submit"
+                className="px-4 py-2 font-bold text-white rounded bg-primary hover:bg-primary-focus focus:outline-none focus:shadow-outline"
+                disabled={submitting || success}
+              >
+                {submitting ? "Submitting..." : "Send Message"}
+              </button>
+            </div>
+          </form>
+        )}
       </motion.div>
     </section>
   );
